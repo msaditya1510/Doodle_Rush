9a156dd32742ea465872a2b33df2f0041be1d84b
msaditya1510 <msaditya15@gmail.com>
2025-09-19 18:17:26 +0530

    modifying for resolving the cors issue

--- Backend/src/main/java/com/rush/doodle/DoodleRushApplication.java
+++ Backend/src/main/java/com/rush/doodle/DoodleRushApplication.java
@@ -2,7 +2,10 @@ package com.rush.doodle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableScheduling
 @SpringBootApplication
@@ -11,5 +14,16 @@ public class DoodleRushApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DoodleRushApplication.class, args);
 	}
+	 @Bean
+	     WebMvcConfigurer corsConfigurer() {
+	        return new WebMvcConfigurer() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/api/**")
+	                        .allowedOrigins("https://doodle-rush.vercel.app/","https://doodle-rush.vercel.app","http://localhost:5173")
+	                        .allowedMethods("*");
+	            }
+	        };
 
+	 }
 }
