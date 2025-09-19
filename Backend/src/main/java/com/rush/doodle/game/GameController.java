029c59674a86cec4046fff08b05570ac075ad4d6
msaditya1510 <msaditya15@gmail.com>
2025-09-19 18:48:06 +0530

    cross origin issues resolved

--- Backend/src/main/java/com/rush/doodle/game/GameController.java
+++ Backend/src/main/java/com/rush/doodle/game/GameController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,7 +25,6 @@ public class GameController {
 	@Autowired private LeaderboardService leaderboardService;
 	
 	
-	@CrossOrigin(origins = "http://localhost:5173")
 	@PostMapping("/startGame/{roomId}/{playerName}")
 	public ResponseEntity<?> startGame(@PathVariable String roomId,@PathVariable String playerName) {
 		return gameService.startGame(roomId,playerName);
